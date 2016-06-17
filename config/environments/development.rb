dc8c818a9da19d02915a36fd287b558f6da6e5eb
Manuel van Rijn <manuel@manuelvanrijn.nl>
2016-06-17 15:55:37 +0200

    ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_cable.url = "ws://localhost:3000/cable"
-  config.action_cable.allowed_request_origins = [/http:\/\/*.*/]
+  config.action_cable.allowed_request_origins = [/http:\/\/localhost*/, /http:\/\/*.ngrok.io/]
 end
